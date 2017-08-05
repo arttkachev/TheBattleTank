15f339ffbe984ef9f63dc5fbbde7158d2f5acd5d
arttkachev <art.tkachev@bk.ru>
2017-08-05 13:15:40 +0300

    The UE4 Project is created. The landscape is made.

---
+++ TheBattleTank/Source/TheBattleTank/TheBattleTankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheBattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEBATTLETANK_API ATheBattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
