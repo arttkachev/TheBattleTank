358b9704e06494780586fbc65376fffa772807fe
arttkachev <art.tkachev@bk.ru>
2017-08-06 19:20:35 +0300

    TankPlayerController is made

---
+++ TheBattleTank/Source/TheBattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "TheBattleTank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
