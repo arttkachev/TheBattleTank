57c40b9c4df65de7fb29e0f2d3679a3cd0b8adcc
arttkachev <art.tkachev@bk.ru>
2017-08-25 00:05:09 +0300

    A tank fires a projectile

--- TheBattleTank/Source/TheBattleTank/Private/Tank.cpp
+++ TheBattleTank/Source/TheBattleTank/Private/Tank.cpp
@@ -1,11 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tank.h"
+#include "Projectile.h"
 #include "Components/ActorComponent.h"
 #include "TankBarrel.h"
 #include "TankTurret.h"
-#include "Projectile.h"
 #include "TankAimingComponent.h"
+
 
 
 
@@ -56,11 +57,13 @@ void ATank::AimAt(FVector HitLocation)
 
 void ATank::Fire()
 {
-	UE_LOG(LogTemp, Warning, TEXT("%f: Tank Fires!"), TankAimingComponent->WorldTimer());
+	
 	if (!Barrel) { return; }
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 	);
+	// Fire the projectile
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
--- TheBattleTank/Source/TheBattleTank/Public/Projectile.h
+++ TheBattleTank/Source/TheBattleTank/Public/Projectile.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
+
+
 
 UCLASS()
 class THEBATTLETANK_API AProjectile : public AActor
@@ -14,14 +17,16 @@ class THEBATTLETANK_API AProjectile : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AProjectile();
+	virtual void Tick(float DeltaTime) override;
+	void LaunchProjectile(float Speed);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
+	
 
 	
 	
