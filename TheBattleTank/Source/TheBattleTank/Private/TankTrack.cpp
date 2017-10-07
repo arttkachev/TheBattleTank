32163ab72891522d07e9a22f2a11ebdbb90144c7
arttkachev <art.tkachev@bk.ru>
2017-10-07 17:32:34 +0300

    Delegate OnHit

--- TheBattleTank/Source/TheBattleTank/Private/TankTrack.cpp
+++ TheBattleTank/Source/TheBattleTank/Private/TankTrack.cpp
@@ -9,6 +9,16 @@ UTankTrack::UTankTrack()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("I'm here, I'm here!"));
+}
+
 
 void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
